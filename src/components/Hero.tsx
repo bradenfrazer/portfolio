bfd4774e54e4cc37187a030d53acdcb8623bf168
Braden Frazer <bradenfrazer@gmail.com>
2023-10-31 12:53:11 -0500

    underline under name in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,11 @@ export const Hero = (props: HeroProps) => {
     <Section variant="hybrid" className="pb-0 pt-20 md:pb-0 md:pt-32">
       <div className="w-full pt-5 md:pt-0">
         <h1 className="mb-4 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
-          Hi, I'm Braden.
+          Hi, I'm{" "}
+          <span className="underline decoration-primary-400 decoration-4 underline-offset-8 lg:decoration-8">
+            Braden
+          </span>
+          .
         </h1>
         <p className="lead text-base sm:text-xl">I help design meet code.</p>
         <Button href="#work">View My Work</Button>
