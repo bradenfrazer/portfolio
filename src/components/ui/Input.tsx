110e4ba3aaf08ec543fd07c7f830e6dec6977011
Braden Frazer <bradenfrazer@gmail.com>
2024-03-28 11:25:00 -0500

    unround corners on inputs/textareas

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -6,7 +6,7 @@ export const Input = (props: InputProps) => {
   return (
     <input
       {...props}
-      className="focus:border-blue focus:shadow-blue w-full rounded border-4 border-gray-950 bg-white p-2  shadow focus:outline-none"
+      className="focus:border-blue focus:shadow-blue w-full border-4 border-gray-950 bg-white p-2  shadow focus:outline-none"
     />
   );
 };
