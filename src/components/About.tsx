049a5eed8d488cc431e5013a246f0e95162bdb6d
Braden Frazer <bradenfrazer@gmail.com>
2024-04-15 20:20:37 -0500

    update for Thesis

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -22,13 +22,13 @@ export const About = () => {
           kinds of tech, and a particular focus in user experience design,
           creating modern user interfaces for websites and applications, while
           also practicing empathy from the user perspective to form an
-          experience that is fun and not frustrating. Currently, I am
-          contracting at{" "}
-          <a href="https://www.bswhealth.com/" target="_blank">
-            Baylor Scott & White Health
+          experience that is fun and not frustrating. Currently, I am working as
+          a UX-focused software engineer at{" "}
+          <a href="https://www.takethesis.com/" target="_blank">
+            Thesis
           </a>
-          , ensuring brand consistency and a design system implementation across
-          the system's public facing websites. If you have any questions or
+          , working to greatly improve the brand's visual identity and translate
+          it into beautiful maintainable code. If you have any questions or
           interest in my work, don't hesitate to contact me through the form
           below!
         </p>
